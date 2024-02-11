2c9c22e099739bccab490779b7ca81054d6dc5ed
dean <dean.moynihan@gmail.com>
2024-02-11 12:24:46 +0000

    dev changes

--- app/components/weather/weather.tsx
+++ app/components/weather/weather.tsx
@@ -8,37 +8,16 @@ export default async function Weather({weatherData}:{weatherData:any}) {
     const weather = await res.json();
 
     const getIconByID = function (id: string) {
-      if (id === "800") {
-        return "sun";
-      }
-
-      if (id.startsWith("8")) {
-        return "cloud";
-      }
-
-      if (id.startsWith("7")) {
-        return "wind";
-      }
-
-      if (id.startsWith("6")) {
-        return "cloud-snow";
-      }
-
-      if (id.startsWith("5")) {
-        return "cloud-rain";
-      }
-
-      if (id.startsWith("3")) {
-        return "cloud-drizzle";
-      }
-
-      if (id.startsWith("2")) {
-        return "cloud-lightning";
-      }
-
+      if (id === "800") return "sun";
+      if (id.startsWith("8")) return "cloud";
+      if (id.startsWith("7")) return "wind";
+      if (id.startsWith("6")) return "cloud-snow";
+      if (id.startsWith("5")) return "cloud-rain";
+      if (id.startsWith("3")) return "cloud-drizzle";
+      if (id.startsWith("2")) return "cloud-lightning";
+      
       return "cloud";
     };
-    //{}
 
     return (
         <>
