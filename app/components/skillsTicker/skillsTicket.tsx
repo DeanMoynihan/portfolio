144fdd8c3cf7af5793bf9dbbca608b90812959a6
dean <dean.moynihan@gmail.com>
2024-02-12 13:04:01 +0000

    skills ticker

---
+++ app/components/skillsTicker/skillsTicket.tsx
@@ -0,0 +1,22 @@
+import FadeOnScroll from "../fadeOnScroll/fadeOnScroll";
+import LineBreak from "../lineBreak/lineBreak";
+
+export default function SkillsTicker({skills}:{skills: string[]}) {
+    const getText = function(){
+        return skills.map((skill, i) => <><a style={{textDecoration: "none"}} key={i}>{skill.toUpperCase()}</a>{" | "}</>)
+    }
+    
+    return (
+        <>
+            <FadeOnScroll>
+                <div style={{minHeight: "150px", display: "flex", alignItems: "center", flexDirection: "row", textWrap: "nowrap", overflowX: "hidden", transform: "translateX(10px)"}}>
+                <div style={{display: "flex", animation: "marquee 30s linear infinite"}}>
+                    <h2>&nbsp;{getText()}</h2>
+                    <h2>&nbsp;{getText()}</h2>
+                </div>
+                </div>
+            </FadeOnScroll>
+            <LineBreak showIcon={false}/>
+        </>
+    );
+}
