6db4b5b7d035bc1c05efc24cebc9afbc3414dcbf
dean <dean.moynihan@gmail.com>
2024-02-12 17:03:03 +0000

    fade delays

--- app/components/skillsTicker/skillsTicket.tsx
+++ app/components/skillsTicker/skillsTicket.tsx
@@ -8,7 +8,7 @@ export default function SkillsTicker({skills}:{skills: string[]}) {
     
     return (
         <>
-            <FadeOnScroll>
+            <FadeOnScroll delay={0}>
                 <div style={{minHeight: "150px", display: "flex", alignItems: "center", flexDirection: "row", textWrap: "nowrap", overflowX: "hidden"}}>
                 <div style={{display: "flex", animation: "marquee 30s linear infinite"}}>
                     <h2>&nbsp;{getText()}</h2>
